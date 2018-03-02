1b16973729a78c6be366acf29393d7b99e8c4542
Deepakumar <gdeepakumar@gmail.com>
2018-03-02 07:05:59 +0530

    barchart

--- src/barchart.js
+++ src/barchart.js
@@ -9,24 +9,30 @@ var margin = {top:20,bottom:20,left:20,right:20};
 function dataReady(data){
     //console.log(data);
 
+    d3.select("svg")
+    .attr("height",height)
+    .attr("width",width);
+
     data.forEach(d => {
-        d.date = d3.timeParse("Y%m%d")(d.date);
-        console.log(d.date);
-        d.date = new Date(d.date);
-        ++d[city];
+         var parseTime = d3.timeParse("%Y%m%d");
+        var d3date = parseTime(d.date);
+        d.date = new Date(d3date);
     });
 
     //Scales
     var xExtent = d3.extent(data,function(d){
-        console.log(d.date);
+        //console.log(d.date);
         return d.date;
     });
-    console.log("X Extent:" + xExtent);
-    var xScale = d3.scaleTime().domain(xExtent).range(margin.left, width - margin.right);
+    //console.log("X Extent:" + xExtent);
+    var xScale = d3.scaleTime().domain(xExtent).range([margin.left, width - margin.right]);
 
     var yExtent = d3.extent(data,d=>d[city]);
-    console.log("y Extent:" + yExtent);
+   //console.log("y Extent:" + yExtent);
     var yScale = d3.scaleLinear().domain(yExtent).range([height - margin.bottom, margin.top]);
+
+    var heightScale = d3.scaleLinear().domain(yExtent)
+    .range([0, height - margin.top - margin.bottom])
 
     //create the rectangles
     var svg = d3.select('svg');
@@ -34,15 +40,31 @@ function dataReady(data){
     .data(data).enter().append('rect')
     .attr('width',5)
     .attr("height",function(d){
-        return height;
+        return heightScale(d[city]);
     })
-    .attr('x',function(d){
-        console.log(xScale(d.date));
+    .attr('x',function(d,i){
         return xScale(d.date);
     })
     .attr('y',function(d){
         return yScale(d[city]);
-    });
+    })
+    .attr('fill', 'blue')
+    .attr('stroke','white')
+
+    //Define Axiis
+    var xAxis = d3.axisBottom()
+    .scale(xScale);
+    var yAxis = d3.axisLeft()
+    .scale(yScale);
+
+    svg.append('g')
+    .attr('transform','translate(' + [0,height-margin.bottom] + ')')
+    .call(xAxis);
+
+    svg.append('g')
+    .attr('transform', 'translate(' + [margin.left, 0] + ')')
+    .call(yAxis);
+    
 
 }
 
